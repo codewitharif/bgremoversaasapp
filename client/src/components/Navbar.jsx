d085a28b5453ff0dde63c5d36d75084dbc7db893
Md Arif <aatif149209@gmail.com>
2025-07-14 15:15:04 +0530

    fixed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import {
 } from "@clerk/clerk-react";
 import { AppContext } from "../context/AppContext";
 import coin from "../assets/coin.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { isSignedIn, user } = useUser();
@@ -32,23 +33,25 @@ const Navbar = () => {
           <div className="flex justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0 flex items-center">
-                <svg
-                  className="h-8 w-8 text-indigo-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                  />
-                </svg>
-                <span className="ml-2 text-xl font-bold text-gray-900">
-                  BG Remover Pro
-                </span>
+                <Link to="/">
+                  <svg
+                    className="h-8 w-8 text-indigo-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    />
+                  </svg>
+                  <span className="ml-2 text-xl font-bold text-gray-900">
+                    BG Remover Pro
+                  </span>
+                </Link>
               </div>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:items-center sm:space-x-8">
